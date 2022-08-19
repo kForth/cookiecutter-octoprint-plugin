6b542deb5b269f8f10be2287082e82cda8aba2f7
Kestin Goforth <kgoforth1503@gmail.com>
2022-08-19 13:48:16 -0400

    Change repo variable name

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 if '{{cookiecutter.initialize_git_repo}}'.strip().lower()[0] == 'y':
     import subprocess
     
-    subprocess.call(['git', 'init', '-b', '{{cookiecutter.default_branch}}'])
+    subprocess.call(['git', 'init', '-b', '{{cookiecutter.repo_branch}}'])
     subprocess.call(['git', 'remote', 'add', 'origin', '{{cookiecutter.plugin_source}}'])
     # subprocess.call(['git', 'add', '*'])
     # subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-    # subprocess.call(['git', 'push', '-u', 'origin', '{{cookiecutter.default_branch}}'])
+    # subprocess.call(['git', 'push', '-u', 'origin', '{{cookiecutter.repo_branch}}'])
