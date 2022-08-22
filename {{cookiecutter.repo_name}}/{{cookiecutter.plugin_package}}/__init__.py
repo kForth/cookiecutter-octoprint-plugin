19c3f6a70ebfe69fb451bd780301e7dea0b60c36
Kestin Goforth <kgoforth1503@gmail.com>
2022-08-22 16:03:03 -0400

    Apply changes from pre-commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.plugin_package}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.plugin_package}}/__init__.py
@@ -1,6 +1,3 @@
-# coding=utf-8
-from __future__ import absolute_import
-
 ### (Don't forget to remove me)
 # This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
 # as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
@@ -10,6 +7,7 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+
 
 class {{ cookiecutter.plugin_identifier | capitalize }}Plugin(octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
